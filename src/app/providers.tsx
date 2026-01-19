f7578ae051e068ca54d73d90caa84f15eb175b8e
Abhinav <avvadali@umass.edu>
2026-01-19 13:34:30 -0500

    fix(provider, nhost-integration): fixed mismatch of nhost dependency versions

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,18 +1,22 @@
 'use client'
-import { PropsWithChildren } from 'react'
+import { PropsWithChildren, useMemo } from 'react'
 import { NhostProvider } from '@nhost/react'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import { nhost } from '@/lib/nhost'
 
 export default function Provider({ children }: PropsWithChildren) {
-  const client = new ApolloClient({
-    uri: nhost.graphql.httpUrl,
-    cache: new InMemoryCache(),
-  })
+  const client = useMemo(
+    () =>
+      new ApolloClient({
+        uri: nhost.graphql.httpUrl,
+        cache: new InMemoryCache(),
+      }),
+    []
+  )
 
   return (
     <NhostProvider nhost={nhost}>
-      <ApolloProvider client = {client}>
+      <ApolloProvider client={client}>
         {children}
       </ApolloProvider>
     </NhostProvider>
--- src/lib/nhost.ts
+++ src/lib/nhost.ts
@@ -1,5 +1,5 @@
 'use client'
-import { NhostClient } from '@nhost/react'
+import { NhostClient } from '@nhost/nhost-js'
 
 export const nhost = new NhostClient({
   subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN!,
